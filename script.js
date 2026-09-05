21416996674cd512acbc675fd4bf10005ec2b7b2
yewengel <yewengel.mesele@astu.edu.et>
2026-09-05 22:52:54 +0400

    updated

---
+++ script.js
@@ -0,0 +1,59 @@
+const taskInput = document.querySelector("#taskInput");
+const addButton = document.querySelector("#addButton");
+const taskList = document.querySelector("#taskList");
+const countText = document.querySelector("#count");
+
+let count = 0;
+
+function addTask() {
+
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    const task = document.createElement("li");
+    task.classList.add("task");
+
+    const text = document.createElement("span");
+    text.textContent = taskText;
+    text.classList.add("task-text");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+
+    task.appendChild(text);
+    task.appendChild(deleteButton);
+
+    taskList.appendChild(task);
+
+    count = count + 1;
+    countText.textContent = count;
+
+    taskInput.value = "";
+
+    text.addEventListener("click", function () {
+        text.classList.toggle("completed");
+    });
+
+    deleteButton.addEventListener("click", function () {
+        task.remove();
+
+        count = count - 1;
+        countText.textContent = count;
+    });
+}
+
+addButton.addEventListener("click", function () {
+    addTask();
+});
+
+taskInput.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        addTask();
+    }
+
+});
